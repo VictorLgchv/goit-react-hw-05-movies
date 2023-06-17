5a4dea0148053bb038dddbadc7a7bc53e2bcd2d9
VictorLgchv <108681513+VictorLgchv@users.noreply.github.com>
2023-06-18 00:52:30 +0300

    Update Reviews.jsx

--- src/components/Reviews.jsx
+++ src/components/Reviews.jsx
@@ -1,6 +1,51 @@
+import { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
+import { fetchReviews } from 'services/fetchData';
+import Loader from './Loader/Loader';
 
 export const Reviews = () => {
   const { movieId } = useParams();
-  return <div>Reviews : {movieId}</div>;
+  const [reviews, setReviews] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      setLoading(true);
+
+      const response = await fetchReviews(movieId);
+
+      try {
+        const data = await response.json();
+        setReviews(data.results);
+      } catch (error) {
+        setError(error);
+      } finally {
+        setLoading(false);
+      }
+    };
+    fetchData();
+  }, [movieId]);
+  return (
+    <>
+      {loading && <Loader />}
+      <ul>
+        {!loading &&
+          reviews.length > 0 &&
+          reviews.map(({ id, author, content }) => (
+            <li key={id}>
+              <div>
+                <h3>Author: {author}</h3>
+                <p>{content}</p>
+              </div>
+            </li>
+          ))}
+      </ul>
+      {!loading && reviews.length === 0 && (
+        <h3>We couldn't find reviews for this film</h3>
+      )}
+
+      {error && <h3>Oops, something went wrong: {error.message}</h3>}
+    </>
+  );
 };
