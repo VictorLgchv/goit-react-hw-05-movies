3d78f14c53731677d728ced0186dee470f86aad4
VictorLgchv <108681513+VictorLgchv@users.noreply.github.com>
2023-06-17 22:53:59 +0300

    added link to back

--- src/components/MovieDetails.jsx
+++ src/components/MovieDetails.jsx
@@ -1,11 +1,17 @@
-import { Link, Outlet, useParams } from 'react-router-dom';
+import { useRef } from 'react';
+import { Link, Outlet, useParams, useLocation } from 'react-router-dom';
 
 export const MovieDetails = () => {
   const { movieId } = useParams();
 
+  const location = useLocation();
+  const backLinkLocationRef = useRef(location.state?.from ?? '/movies')
+
+
   return (
     <>
       <div>Movie Details : {movieId}</div>
+      <Link to={backLinkLocationRef.current}>Back</Link>
       <ul>
         <li>
           <Link to="cast">Cast</Link>
