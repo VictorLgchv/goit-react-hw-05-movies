5bac433125ca3dd6ee0ad31a365b031d85b5146d
VictorLgchv <108681513+VictorLgchv@users.noreply.github.com>
2023-06-18 11:25:05 +0300

    added cast

--- src/components/Cast.jsx
+++ src/components/Cast.jsx
@@ -1,6 +1,62 @@
+import { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
+import { fetchCast } from 'services/fetchData';
+import Loader from './Loader/Loader';
+import { defaultImg } from './defaultImg';
 
 export const Cast = () => {
   const { movieId } = useParams();
-  return <div>Cast : {movieId}</div>;
+  const [cast, setCast] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    setLoading(true);
+
+    const fetchData = async () => {
+      const response = await fetchCast(movieId);
+
+      try {
+        const data = await response.json();
+        setCast(data.cast);
+      } catch (error) {
+        setError(error);
+      } finally {
+        setLoading(false);
+      }
+    };
+    fetchData();
+  }, [movieId]);
+
+  return (
+    <>
+      {loading && <Loader />}
+      <ul>
+        {!loading &&
+          cast.length > 0 &&
+          cast.map(({ id, name, character, profile_path }) => (
+            <li key={id}>
+              <div>
+                <img
+                  width={80}
+                  src={
+                    profile_path
+                      ? `https://image.tmdb.org/t/p/w200${profile_path}`
+                      : defaultImg
+                  }
+                  alt={name}
+                />
+                {name}
+                <p>Character: {character}</p>
+              </div>
+            </li>
+          ))}
+      </ul>
+      {!loading && cast.length === 0 && (
+        <h3>We have no info about cast of this film</h3>
+      )}
+
+      {error && <h3>Oops, something went wrong: {error.message}</h3>}
+    </>
+  );
 };
---
+++ src/components/defaultImg.jsx
@@ -0,0 +1,2 @@
+export const defaultImg =
+  'https://static.vecteezy.com/system/resources/previews/005/337/799/original/icon-image-not-found-free-vector.jpg';
